120ba27e0ce2c630a5119d655ce1f329aaac264c
laniakita <101172492+laniakita@users.noreply.github.com>
2024-12-18 13:48:53 -1000

    rewrite lib

--- crates/data_factory/src/mini_profile.rs
+++ crates/data_factory/src/mini_profile.rs
@@ -3,9 +3,10 @@ use crate::utils::set_panic_hook;
 use web_sys::console;
 
 #[wasm_bindgen]
-pub fn to_mini_profile() {
+pub fn to_mini_profile(profile_data: JsValue) {
   // init our panic hook
   set_panic_hook();
 
   console::log_1(&"pretending to create mini profile from data.".into());
+  console::log_1(&profile_data);
 }
