673f17e5e48c73318e68ceaf9c2e41cd8499ce18
laniakita <101172492+laniakita@users.noreply.github.com>
2024-12-20 00:30:04 -1000

    added frosted glass effect inspired by Josh W. Comeau

--- extension/src/App.svelte
+++ extension/src/App.svelte
@@ -23,7 +23,7 @@
 </script>
 
 <main
-  class="w-[20rem] min-h-[30rem] bg-slate-950 text-slate-300 flex flex-col justify-start items-center"
+  class="w-[20rem] min-h-[30rem] bg-black text-slate-300 flex flex-col justify-start items-center overflow-hidden"
 >
   {#if shouldDisplayProfile && miniProfileData.banner !== null}
     <div class="">
@@ -36,22 +36,76 @@
   {/if}
 
   {#if shouldDisplayProfile}
-    <div class="px-14 pb-14 {miniProfileData.banner ? '-mt-10' : 'pt-14'} {miniProfileData.description === null ? 'flex flex-col justify-center items-center' : ''}">
-      <img
-        src={miniProfileData.avatar}
-        alt="{miniProfileData.displayName ?? miniProfileData.handle} profile"
-        class="w-20 h-20 rounded-full border-2 border-slate-300"
-      />
-      <h1 class="text-3xl">{miniProfileData.displayName}</h1>
-      <h2 class="text-lg {!miniProfileData.displayName && 'mt-2'}">@{miniProfileData.handle}</h2>
-      <p>{miniProfileData.description}</p>
-      <button class="bg-blue-500 rounded-full w-full min-h-10 py-2 border border-blue-300 hover:border-blue-800 text-center mt-10 font-mono font-semibold hover:bg-blue-300 hover:text-blue-800 px-4 text-balance">
-        follow <span class="whitespace-nowrap">{miniProfileData.displayName ?? miniProfileData.handle}</span> on bluesky
-      </button>
+    <div class="relative ">
+      <div class="backdrop"></div>
+      <div class="backdrop-edge"></div>
+
+      <div
+        class="relative px-14 pb-24 z-50 {miniProfileData.banner
+          ? '-mt-10'
+          : 'pt-14'} flex flex-col justify-center items-center gap-y-4 h-full"
+      >
+        <div
+          class="flex flex-col justify-center {miniProfileData.description? 'items-start' :'items-center'} gap-y-1 text-slate-400"
+        >
+          <img
+            src={miniProfileData.avatar}
+            alt="{miniProfileData.displayName ??
+              miniProfileData.handle} profile"
+            class="w-20 h-20 rounded-full border-2 border-slate-300"
+          />
+
+          <h1 class="text-2xl font-semibold text-slate-200">
+            {miniProfileData.displayName}
+          </h1>
+          <button class="hover:text-blue-500 hover:underline">
+            <h2 class="{miniProfileData.displayName ? '' : 'mt-2'} text-base">
+              @{miniProfileData.handle}
+            </h2>
+          </button>
+          <p class="text-sm">{miniProfileData.description}</p>
+        </div>
+        
+        <button
+          class="bg-blue-500 text-blue-200 rounded-full w-full min-h-10 py-2 border border-blue-200 hover:border-blue-800 text-center font-mono font-semibold hover:bg-blue-300 hover:text-blue-800 px-4 text-balance"
+        >
+          follow <span class="whitespace-nowrap"
+            >{miniProfileData.displayName ?? miniProfileData.handle}</span
+          > on bluesky
+        </button>
+      </div>
     </div>
   {:else}
     <div class="p-20">
       <h3 class="text-lg">{title}</h3>
     </div>
   {/if}
+
 </main>
+
+<style>
+  .backdrop {
+    position: absolute;
+    inset: 0;
+    height: 200%;
+    transform: translateY(-50%);
+    backdrop-filter: blur(18px) brightness(150%) saturate(160%);
+    mask-image: linear-gradient(to top, black 0% 50%, transparent 50% 100%);
+    pointer-events: none;
+  }
+  .backdrop-edge {
+    --thickness: 6px;
+    position: absolute;
+    inset: 0;
+    height: 100%;
+    transform: translateY(-100%);
+    background: hsl(0deg 0% 100% / 0.1);
+    backdrop-filter: blur(8px) brightness(180%) saturate(120%);
+    mask-image: linear-gradient(
+      to top,
+      black 0,
+      black var(--thickness),
+      transparent var(--thickness)
+    );
+  }
+</style>
